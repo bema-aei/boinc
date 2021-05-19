932a3633675766658b784d1ab5d9304d90d52d38
Bernd Machenschalk <bernd.machenschalk@aei.mpg.de>
2021-05-19 17:28:59 +0200

    Xcode: added detect_rosetta_cpu as build target (x86_64 arch only)

---
+++ mac_build/detect_rosetta_cpu/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  detect_rosetta_cpu
+//
+//  Created by Bernd Machenschalk on 19.05.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
